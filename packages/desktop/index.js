40018ae90dff4d40f1d6fd07d96cacb37e495ad3
Aayushd18 <aayushd@iitbhilai.ac.in>
2021-07-17 16:08:05 +0530

    modified front Ui & tracker.js

--- packages/desktop/index.js
+++ packages/desktop/index.js
@@ -1,15 +1,7 @@
-import activeWin from 'active-win';
-import ActiveWindowObject from "./tracker.js";
-import path from 'path';
+import ActiveWindowObject from './tracker.js';
 
-// log active window twice (this would give error in active-win from npm)
-//console.log(activeWin.sync().title);
-//console.log(activeWin.sync().title);
-
-const __dirname = path.resolve();
-const filePath = path.join(__dirname, "activites.json")
-const interval = 2000
-
+const filePath = process.cwd() + '/activites.json';
+const interval = 2000;
 
 const activeWindowObject = new ActiveWindowObject(filePath, interval);
 activeWindowObject.initialize();
