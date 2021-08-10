46e7df358f1ab33c1a4bda1edd4e34c9adfffd1e
KShivendu <shivendu@iitbhilai.ac.in>
2021-08-10 11:05:22 +0530

    server/app/usage: Add new endpoint for weekly data

--- packages/server/controllers/app.controller.js
+++ packages/server/controllers/app.controller.js
@@ -18,3 +18,38 @@ export const all_apps = async (req, res) => {
 
 	res.json(apps);
 };
+
+export const app_usage = async (req, res) => {
+	const query = [
+		{
+			$project: {
+				dayOfWeek: {
+					$dayOfWeek: {
+						$toDate: '$startTime',
+					},
+				},
+				name: '$name',
+				timeSpent: {
+					$subtract: ['$endTime', '$startTime'],
+				},
+			},
+		},
+		{
+			$group: {
+				_id: '$dayOfWeek',
+				name: {
+					$addToSet: '$name',
+				},
+				duration: {
+					$sum: '$timeSpent',
+				},
+			},
+		},
+		{ $sort: { id: 1, _id: 1 } },
+	];
+	if (req.query.name) query.unshift({ $match: { name: req.query.name } });
+
+	const apps = await Activity.aggregate(query);
+
+	res.json(apps);
+};
--- packages/server/routes/app.routes.js
+++ packages/server/routes/app.routes.js
@@ -4,5 +4,6 @@ import * as app_controller from '../controllers/app.controller.js';
 const router = express.Router();
 
 router.get('/', app_controller.all_apps);
+router.get('/usage', app_controller.app_usage);
 
 export default router;
