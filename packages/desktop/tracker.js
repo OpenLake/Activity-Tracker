274f856dfcd7a49bc39ee490fcaeae30424b3874
Ambar Mutha <ambar.ytl@gmail.com>
2021-08-17 19:53:04 +0530

    Separate storage from watcher

--- packages/desktop/tracker.js
+++ packages/desktop/tracker.js
@@ -1,13 +1,14 @@
 import activeWin from 'active-win';
-import axios from 'axios';
-import { existsSync, writeFileSync, readFileSync } from 'fs';
-import getIcon from './getIcon.js';
 
 export class ActiveWindowWatcher {
-	constructor(filePath, interval) {
+	/**
+	 * @param {number} interval Polling interval
+	 * @param {(activity) => void} changeCallback
+	 */
+	constructor(interval = 1000, changeCallback) {
 		this.startTime = null;
 		this.app = null;
-		this.filePath = filePath;
+		this.changeCallback = changeCallback;
 		this.interval = interval;
 	}
 
@@ -16,51 +17,24 @@ export class ActiveWindowWatcher {
 	 * Collecting data of the window which will be active
 	 */
 	storeTime() {
-		const rawData = readFileSync(this.filePath);
-		const file = JSON.parse(rawData);
 		const endTime = Date.now();
 		const startTime = this.startTime;
 
-		//writing the json file
 		const {
 			owner: { name },
 			title,
 			url,
 		} = this.app;
 
-		function appDataObject(startTime, endTime) {
-			return {
-				name,
-				title: title ?? 'No data',
-				url,
-				startTime,
-				endTime,
-			};
-		}
+		const data = {
+			name,
+			title: title ?? 'No Title',
+			url,
+			startTime,
+			endTime,
+		};
 
-		const data = appDataObject(startTime, endTime);
-
-		console.log(getIcon(name));
-
-		// Making a post request to '/store'
-		axios
-			.post('http://localhost:3000/activities', {
-				name: name,
-				title: title,
-				startTime: startTime,
-				endTime: endTime,
-			})
-			.then(res => {
-				console.log(`statusCode: ${res.status}`);
-				console.log(res.data);
-			})
-			.catch(error => {
-				console.error(error);
-			});
-
-		file.push(data);
-
-		writeFileSync(this.filePath, JSON.stringify(file, null, 2));
+		this.changeCallback(data);
 	}
 
 	/**
@@ -86,12 +60,6 @@ export class ActiveWindowWatcher {
 	}
 
 	initialize() {
-		const isFilePresent = existsSync(this.filePath);
-
-		if (!isFilePresent) {
-			writeFileSync(this.filePath, '[]');
-		}
-
 		this.tracker();
 	}
 }
