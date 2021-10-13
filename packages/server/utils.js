f661016747a96173e3e6c2be2349254f739ca5e1
Aayushd18 <aayushd@iitbhilai.ac.in>
2021-10-13 18:58:09 +0530

    add email sender feature

--- packages/server/utils.js
+++ packages/server/utils.js
@@ -3,7 +3,7 @@ import googleapis from 'googleapis';
 const { google } = googleapis;
 const OAuth2 = google.auth.OAuth2;
 
-//new Authorized client
+// new Authorized client
 const OAuth2Client = new OAuth2(
 	process.env.CLIENT_ID,
 	process.env.CLIENT_SECRET,
@@ -14,10 +14,10 @@ OAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 export async function emailSender(receiverEmail) {
 	// TODO: Send actual OTP using nodemailer
 	try {
-		//getting the access token each time to send emails
+		// getting the access token each time to send emails
 		const accessToken = await OAuth2Client.getAccessToken();
 
-		//Create a transport meduim for emails
+		// Create a transport meduim for emails
 		const transport = nodemailer.createTransport({
 			service: 'gmail',
 			auth: {
@@ -33,7 +33,7 @@ export async function emailSender(receiverEmail) {
 			},
 		});
 
-		//email option
+		// email option
 		const mailOptions = {
 			from: 'Activity Tracker <activitytrackercpat@gmail.com>',
 			to: receiverEmail,
@@ -42,7 +42,7 @@ export async function emailSender(receiverEmail) {
 			html: '<b>Your OTP is 123456<b/>',
 		};
 
-		//Send the emails
+		// Send the emails
 		const info = await transport.sendMail(mailOptions);
 		return info;
 	} catch (error) {
