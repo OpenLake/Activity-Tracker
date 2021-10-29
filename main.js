2170910b8c4858e00cc62996be5a2be12614f61c
Ambar Mutha <ambar.ytl@gmail.com>
2021-10-29 12:45:09 +0530

    Use require instead of fork

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-const { fork } = require('child_process');
-
-const tracker = fork('packages/desktop/.babel/index.js');
-const server = fork('packages/server/.babel/app.js');
-
-process.on('exit', () => {
-	server.kill('SIGINT');
-	tracker.kill('SIGINT');
-});
+require('./packages/desktop/.babel');
+require('./packages/server/.babel/app');
