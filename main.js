e43d67c004af3117626db425eae7029ebf9156c6
KShivendu <shivendu@iitbhilai.ac.in>
2021-10-30 12:37:53 +0530

    feat(bundle): Autolaunch binary on system startup

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 require('./packages/desktop/.babel');
 require('./packages/server/.babel/app');
+
+const isBundled = __dirname.startsWith('/snapshot');
+
+if (isBundled) {
+	const AutoLaunch = require('auto-launch');
+	const autolauncher = new AutoLaunch({
+		name: 'Activity-Tracker',
+		path: process.execPath,
+	});
+	autolauncher
+		.isEnabled()
+		.then(isEnabled => {
+			if (isEnabled) {
+				return;
+			}
+			autolauncher.enable();
+			console.log('Enabled autolaunch');
+		})
+		.catch(err => {
+			console.error("Couldn't enable autolaunch");
+			console.error(err);
+		});
+}
