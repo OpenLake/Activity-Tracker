91c1b982a4f9752d09ba1011ee873ca8b83a56a8
Kumar Shivendu <shivendu@iitbhilai.ac.in>
2021-11-08 14:34:20 +0530

    feat(bundle): Auto-launch binary on system startup (#41)
    * feat(bundle): Autolaunch binary on system startup
    
    * feat(bundle): Don't display running script on system startup
    
    * Correctly detect isBundled on windows
    
    Co-authored-by: Ambar Mutha <ambar.ytl@gmail.com>

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
 require('./packages/desktop/.babel');
 require('./packages/server/.babel/app');
+
+const isBundled = ['/snapshot', 'C:\\snapshot'].some(prefix =>
+	__dirname.startsWith(prefix),
+);
+
+if (isBundled) {
+	const AutoLaunch = require('auto-launch');
+	const autolauncher = new AutoLaunch({
+		name: 'Activity-Tracker',
+		path: process.execPath,
+		isHidden: true,
+	});
+	autolauncher
+		.isEnabled()
+		.then(isEnabled => {
+			if (isEnabled) {
+				return;
+			}
+			autolauncher.enable();
+			console.log('Enabled autolaunch');
+		})
+		.catch(err => {
+			console.error("Couldn't enable autolaunch");
+			console.error(err);
+		});
+}
