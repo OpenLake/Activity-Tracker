628d1298502d83b3733ac00bd4b315837f93efdf
Lalit <lalitkumarsingh3716@gmail.com>
2021-12-10 12:40:35 +0530

    Fixed: durationToString method
    Now DurationToString Method works as intended but now this method takes duration only in milliseconds.

--- packages/desktop/src/utils.js
+++ packages/desktop/src/utils.js
@@ -1,4 +1,34 @@
-export const durationToString = (duration, isInMinutes = false) => {
-	if (!isInMinutes) duration = Math.floor(duration / 1000 / 60);
-	return `${Math.floor(duration / 60)}h ${duration % 60}m`;
+const parseDuration = durationInSeconds => {
+  const days = Math.floor(durationInSeconds / (60 * 60 * 24));
+  const hours = Math.floor(durationInSeconds / (60 * 60)) % 24;
+  const minutes = Math.floor((durationInSeconds % 3600) / 60);
+  const seconds = durationInSeconds % 60;
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
 };
+
+const formatDuration = ({ days, hours, minutes, seconds }) => {
+  const duration = [];
+  if (days) {
+    duration.push(`${days}d `);
+  }
+  if (hours) {
+    duration.push(`${hours}hr `);
+  }
+  if (minutes) {
+    duration.push(`${minutes}m `);
+  }
+  if (seconds) {
+    duration.push(`${seconds}s`);
+  }
+  return duration.splice(0, 2).join("");
+};
+
+export const durationToString = (duration) => {
+  const durationInSeconds = Math.floor(duration/1000);
+  return formatDuration(parseDuration(durationInSeconds));
+}
