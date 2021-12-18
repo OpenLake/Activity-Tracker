54c46d466359a4283db2bdf21f74d0c34ca9eecb
Ambar Mutha <ambar.ytl@gmail.com>
2021-12-18 16:06:44 +0530

    Setup desktop tests with jest + testing-library

---
+++ packages/desktop/jest.config.js
@@ -0,0 +1,9 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+export default {
+	testEnvironment: 'jsdom',
+};
