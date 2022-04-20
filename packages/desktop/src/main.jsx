012da57b1a40871cfb67dc5c8472b3b4f452d88e
Ambar Mutha <ambar.ytl@gmail.com>
2022-04-20 18:01:55 +0530

    react 18

--- packages/desktop/src/main.jsx
+++ packages/desktop/src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(
-	<React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+	<StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
+	</StrictMode>,
 );
